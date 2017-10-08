7c674b67ade15680da263053ac26cf57b6376550
akash5324 <akash.yadav5324@gmail.com>
2017-10-09 02:39:04 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path=require('path');
+const publicPath=path.join(__dirname,'../public');
+const express=require('express');
+
+
+console.log(__dirname,'../public');
+console.log(publicPath);
+const port=process.env.PORT || 3000;
+var app=express();
+app.use(express.static(publicPath));
+app.listen(port,()=>{
+
+console.log(`server is listening to port number ${port}`);
+
+});
